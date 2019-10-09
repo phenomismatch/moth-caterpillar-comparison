b21df6f615f056b69de00d4108b465448533b5da
Ni <georgeni2442@gmail.com>
2019-10-09 15:31:28 -0400

    Added plots for coweeta data phenology and tree survey observations

--- coweeta_pheno_analysis.R
+++ coweeta_pheno_analysis.R
@@ -10,22 +10,50 @@ site_filter<-function(field_year,field_plot,threshold_value){
   merged<-merge_fun(cow_surv,cow_arth)
   date<-date_change(merged)
 }
-  
-   
+
+par(mfrow=c(5,2))
+for (i in 2010:2018){
+  Tree<-site_filter(i, "BB", 50)%>%
+  group_by(PlantSpecies,julianweek)%>%
+   summarize(count=n())%>%
+  spread(PlantSpecies, count)
+  plot(x=Tree$julianweek, y=Tree$`American chestnut`,xlab="Julian Week", ylab="nSurveys", type="b")
+  points(x=Tree$julianweek, y=Tree$`Red maple`, col="red", type ="b")
+  points(x=Tree$julianweek, y=Tree$`Red oak`, col="blue", type="b")
+  points(x=Tree$julianweek, y=Tree$`Striped maple`, col="green", type= "b")
+}
+
 par(mfrow=c(3,2))
+for (i in 2010:2015){
+Tree<-site_filter(i, "BS", 50)%>%
+  group_by(PlantSpecies,julianweek)%>%
+  summarize(count=n())  
+plot(x=Tree$julianweek,y=Tree$count, xlab="Julian Week", ylab="nSurveys", type="b")
+}
 
 
-BB_50_2010<-meanDensityByWeek(surveyData = site_filter(2010,"BB",50),plot = TRUE,new=TRUE)
-BB_100_2010<-meanDensityByWeek(surveyData = site_filter(2010,"BB",100),plot = TRUE,new=FALSE, color='blue', lty=2)
+par(mfrow=c(3,2))
+cow_plots<-for (i in 2010:2018){
+  tryCatch({
+    meanDensityByWeek(surveyData = site_filter(i,"BS",50),plot = TRUE,new=TRUE, xlab="Julian Week", ylab="Frac of Surveys")
+    meanDensityByWeek(surveyData = site_filter(i,"BS",100),plot = TRUE,new=FALSE, color='blue', lty=2)
+    meanDensityByWeek(surveyData = site_filter(i,"BB",50),plot = TRUE,new=TRUE,xlab="Julian Week", ylab="Frac of Surveys")
+    meanDensityByWeek(surveyData = site_filter(i,"BB",100),plot = TRUE,new=FALSE, color='blue', lty=2)
+ 
+    },error=function(e){}
+  )
+}
 
-BS_50_2010<-meanDensityByWeek(surveyData = site_filter(2010,"BS",50),plot = TRUE,new=TRUE)
-BS_100_2010<-meanDensityByWeek(surveyData = site_filter(2010,"BS",100),plot = TRUE,new=FALSE, color='blue', lty=2)
 
+for (tree in c(American-Chestnut, Striped-Maple, Red-Oak, Red-Maple)){
+  
+  
+}
 
 # Write files
-write.table(branches[, !names(branches) %in% "Branch"], "Plants_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
-write.table(cowsurvs[, !names(cowsurvs) %in% "Branch"], "Survey_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
-write.table(cowarths, "ArthropodSighting_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
+#write.table(branches[, !names(branches) %in% "Branch"], "Plants_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
+#write.table(cowsurvs[, !names(cowsurvs) %in% "Branch"], "Survey_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
+#write.table(cowarths, "ArthropodSighting_Coweeta_2010_BB.txt", sep = '\t', row.names = F)
 
 
 # BS 2002-2018, BB 2003-2018, RK 2002-2008
