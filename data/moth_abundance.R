3778914e40bb56e5904bf432d1194cbaf9d6c862
Ni <georgeni2442@gmail.com>
2019-10-15 14:30:28 -0400

    Organizing moth data

--- data/moth_abundance.R
+++ data/moth_abundance.R
@@ -9,4 +9,24 @@ coweeta<- read.table('c:/git/moth-caterpillar-comparison/data/coweeta_cats.txt',
 #Moth abundance data seems to be clean and complete, has NA's for the morpho species but don't think I need to worry about that.
 #Coweeta data is filtered for 2010 and beyond and each observation has the appropriate amount of leaves (n>40 leaves). 
 #Still need to look into filtering Coweeta data for duplicates of 0 in both records (Same year/yearday/sample, etc, but 0 in both records)
+#Look at two ways to organize the moth data, one where the years are all aggregated and the julian day records are matched together
+#The other way is to organize by the lunar phases, so to create some function that takes the julian day past new moon and subtract from the first day of that year.
+
+moth_by_day<-moth%>%
+  group_by(julian.day)%>%
+  summarize(nCount=sum(daily.moth.species))
+
+moth_by_week<-moth%>%
+  mutate(JulianWeek=7*floor((julian.day)/7)+4)%>%
+  select(JulianWeek,daily.moth.species)%>% 
+  group_by(JulianWeek)%>%
+  summarize(count=n(daily.moth.species))
+ 
+
+
+  
+plot(moth_by_day)
+lines()
+
+plot(moth_by_week)
 
